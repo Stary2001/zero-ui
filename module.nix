fbf9b94e7363bef0d0ad2338efe2670914a7f573
Chloe Bethel <chloe@9net.org>
2023-03-27 21:34:52 +0100

    feat: secret file

--- module.nix
+++ module.nix
@@ -1,10 +1,18 @@
 flake: { pkgs, lib, config, ... }: with lib;
+let cfg = config.services.zero-ui; in 
 {
   options.services.zero-ui = {
     enable = mkEnableOption ("zero-ui ZeroTier Controller UI");
+    controllerSecretFile = mkOption {
+      type = types.path;
+      # No default!
+      description = lib.mdDoc ''
+        Path to file that contains an environment variable in the format ZU_CONTROLLER_TOKEN=[zerotier controller token].
+      '';
+    };
   };
 
-  config = mkIf config.services.zero-ui.enable {
+  config = mkIf cfg.enable {
     systemd.services.zero-ui = {
       description = "ZeroTier Controller UI";
       environment = {
@@ -21,7 +29,7 @@ flake: { pkgs, lib, config, ... }: with lib;
         WorkingDirectory = "${flake.packages.${pkgs.stdenv.hostPlatform.system}.default}/libexec/zero-ui/backend";
         User = "zero-ui";
         StateDirectory = "zero-ui";
-        # TODO: add ZU_CONTROLLER_TOKEN somehow
+        EnvironmentFile = cfg.controllerSecretFile;
       };
     };
 
